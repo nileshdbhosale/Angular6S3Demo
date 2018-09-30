55d644c82a6efb8f5defc85f9957ad7443d26a90
Nilesh Bhosale <nilesh.d.bhosale@gmail.com>
2018-09-29 21:38:39 -0400

    added code for s3 connection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as AWS from 'aws-sdk';
+import { Key } from 'protractor';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 's3-demo';
+  title = 'Angular4-s3-demo';
+  //AWS = require('aws-sdk');
+  s3Bucket = new AWS.S3();
+
+  constructor(){
+    console.log("Inside Constructor !!!");
+  }
+
+public GetS3File(){
+  console.log("Inside Get File Method");
+    var params = {Bucket: 'myfirstawsbucketdemo', Key: 'text.txt'};
+    this.s3Bucket.getSignedUrl('getObject', params, function(err, url){
+            if(err){
+                console.log(err);
+            }else {
+                console.log(url);
+            }
+    })
+  }
 }
